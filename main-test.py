bf75099d819565dfacf48f0daa782b69f78cc57f
TRAORE Abdoul <abdoul.traore@data354.co>
2022-04-05 17:38:12 +0000

    add circleci config

--- main-test.py
+++ main-test.py
@@ -2,7 +2,7 @@ from main import Add
 
 def TestAdd():
     assert Add(2,3)== 5
-    assert Add(2,3)== 6
+    assert Add(5,5)== 10
     print("add fonction work corrrectly")
 
 if __name__=="__main__":
